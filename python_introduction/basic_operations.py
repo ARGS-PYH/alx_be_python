e9d6bf3d5ab67a35975866d1d68ecd52653b0627
Dre <daanimasaun@gmail.com>
2025-05-22 22:08:37 +0100

    initial commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+addtion = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+print ("Addition of 10 and 5 is " + str(addtion))
+print ("Subtraction of 10 and 5 is " + str(subtraction))
+print ("Multiplication of 10 and 5 is " + str(multiplication))
