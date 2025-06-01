288f3923f8512c4d573b50d7cc13992fc443ffc5
Dre <daanimasaun@gmail.com>
2025-06-01 21:57:41 +0100

    adding weather advice program

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,9 +1,9 @@
 weather = input("What's the weather like today? (sunny/rainy/cold): ")
-if weather.lower() == "sunny":
+if weather == "sunny":
     print(f"Wear a t-shirt and sunglasses")
 elif weather.lower() == "rainy":
     print(f"Don't forget your umbrella and a raincoat")
-elif weather.lower() == "cold":
+elif weather == "cold":
     print(f"Make sure to wear a warm coat and a scarf")
 else:
     print("Sorry, I don't have recommendations for this weather")
