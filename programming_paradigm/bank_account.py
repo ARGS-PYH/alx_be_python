be3934c2240654adc4471f2f4bf9e0c31980a721
Dre <daanimasaun@gmail.com>
2025-06-15 23:13:58 +0100

    Update made on Bank app

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,22 +1,19 @@
 class BankAccount:
-    def __init__(self, account_balance=0):
-        self.account_balance = account_balance
-
+    def __init__(self, initial_balance=0):
+        self.initial_balance = initial_balance
     def deposit(self, amount):
-        #self.account_balance = amount + self.account_balance
-        self.account_balance += amount
-#        if amount > 0:
-#           self.account_balance += amount
-#      else:
-#         print("You cannot deposit a negative amount or Zero amount")
-
+        if amount > 0:
+            self.initial_balance  += amount
+        else:
+            print("Deposit amount must be positive.")
     def withdraw(self, amount):
-        if amount > self.account_balance:
+        if amount <= 0:
+            return False
+        if amount > self.initial_balance:
             return False
         else:
-            self.account_balance -= amount
+            self.initial_balance -= amount
             return True
+        
     def display_balance(self):
-        print(f"Your Current account balance is {self.account_balance}")
-
-
+        print(f"Current Balance: ${self.initial_balance:.2f}")
